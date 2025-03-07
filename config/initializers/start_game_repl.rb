bfc761b893e488724bc7fa8c2ae0ca466a340742
Robert O Butts <robert.o.butts@gmail.com>
2025-03-07 17:04:00 +0000

    Change to use ruby-text-game gem

--- config/initializers/start_game_repl.rb
+++ config/initializers/start_game_repl.rb
@@ -1,60 +1,57 @@
-require_relative "../../lib/game_service/idgen"
-require_relative "../../lib/game_service/room"
-require_relative "../../lib/game_service/npc"
-require_relative "../../lib/game_service/world"
-require_relative "../../lib/game_service/object"
-require_relative "../../lib/game_service/direction"
-require_relative "../../app/helpers/application_helper"
+require_relative '../../app/helpers/application_helper'
 
-# require_relative '../../world'
-# require_relative 'object'
-# require_relative 'room'
-# require_relative 'command'
-# require_relative 'direction'
-# require_relative 'events'
-# require_relative 'npc'
+require 'ruby-text-game'
 
-puts "DEBUG puts start_game_repl called"
+puts 'DEBUG making gem world'
 
-ii = IdGenerator.new
+gii = RubyTextGame::IdGenerator.new
 
-id = ii.get
-puts "id: #{id}"
+gid = gii.get
+puts "gem id: #{gid}"
 
-id = ii.get
-puts "id: #{id}"
+puts 'DEBUG puts start_game_repl called'
 
-puts "DEBUG puts start_game_repl called"
+# ii = IdGenerator.new
 
+# id = ii.get
+# puts "id: #{id}"
+
+# id = ii.get
+# puts "id: #{id}"
+
+puts 'DEBUG puts start_game_repl called'
 
 def init_world
-  world = World.new
+  world = RubyTextGame::World.new
+  world.Start # TODO: add world.Stop? Where?
   ApplicationHelper.set_world(world)
 
-  puts "DEBUG init_world 0"
+  puts 'DEBUG init_world 0'
 
-  roomA = Room.new(world.new_id, "A small garden", "This garden isn't very large.", "The garden smells like wildflowers")
-  roomB = Room.new(world.new_id, "A large kitchen", "This kitchen is quite large. Pots hang on the walls, and something smells good.", "The garden smells like wildflowers")
+  roomA = RubyTextGame::Room.new(world.new_id, 'A small garden', "This garden isn't very large.",
+                                 'The garden smells like wildflowers')
+  roomB = RubyTextGame::Room.new(world.new_id, 'A large kitchen',
+                                 'This kitchen is quite large. Pots hang on the walls, and something smells good.', 'The garden smells like wildflowers')
 
-  world.link_rooms(roomA, Direction::EAST, roomB)
+  world.link_rooms(roomA, RubyTextGame::Direction::EAST, roomB)
 
-  player = world.make_player("rob", roomA) # TODO make users createable
+  player = world.make_player('rob', roomA) # TODO: make users createable
 
-  sword = Sword.new(world.new_id, "a short sword", "This is a very shiny sword.", 42)
-  cup = Objectt.new(world.new_id, "cup", "an ornate silver cup", "This cup is very ornate and silver.")
+  sword = RubyTextGame::Sword.new(world.new_id, 'a short sword', 'This is a very shiny sword.', 42)
+  cup = RubyTextGame::GameObject.new(world.new_id, 'cup', 'an ornate silver cup', 'This cup is very ornate and silver.')
 
   player.add_item(sword)
   player.add_item(cup)
 
-  world_key = make_world_key(world)
+  world_key = RubyTextGame.make_world_key(world)
   player.add_item(world_key)
 
-  puts "DEBUG init_world 98"
+  puts 'DEBUG init_world 98'
 
-  goblin = NPC.new(world.new_id, "goblin", "a pungent goblin", "This goblin is quite rank.", true)
+  goblin = RubyTextGame::NPC.new(world.new_id, 'goblin', 'a pungent goblin', 'This goblin is quite rank.', true)
   player.add_item(goblin)
 
-  puts "DEBUG init_world 99"
+  puts 'DEBUG init_world 99'
 end
 
 init_world

b3ec3e816806c3ab306d5fe5169e54149844501a
Robert O Butts <robert.o.butts@gmail.com>
2025-03-10 13:26:37 +0000

    Change init to use logger

--- config/initializers/start_game_repl.rb
+++ config/initializers/start_game_repl.rb
@@ -2,31 +2,20 @@ require_relative '../../app/helpers/application_helper'
 
 require 'ruby-text-game'
 
-puts 'DEBUG making gem world'
+# Rails.logger.info 'making gem world'
+# gii = RubyTextGame::IdGenerator.new
+# gid = gii.get
+# Rails.logger.info "gem id: #{gid}"
 
-gii = RubyTextGame::IdGenerator.new
-
-gid = gii.get
-puts "gem id: #{gid}"
-
-puts 'DEBUG puts start_game_repl called'
-
-# ii = IdGenerator.new
-
-# id = ii.get
-# puts "id: #{id}"
-
-# id = ii.get
-# puts "id: #{id}"
-
-puts 'DEBUG puts start_game_repl called'
+Rails.logger.info 'start_game_repl called'
 
 def init_world
+  Rails.logger.info 'init_world starting'
   world = RubyTextGame::World.new
   world.start # TODO: add world.Stop? Where?
   ApplicationHelper.set_world(world)
 
-  puts 'DEBUG init_world 0'
+  Rails.logger.info 'init_world set world'
 
   roomA = RubyTextGame::Room.new(world.new_id, 'A small garden', "This garden isn't very large.",
                                  'The garden smells like wildflowers')
@@ -46,12 +35,12 @@ def init_world
   world_key = RubyTextGame.make_world_key(world)
   player.add_item(world_key)
 
-  puts 'DEBUG init_world 98'
+  Rails.logger.info 'init_world creating goblin'
 
   goblin = RubyTextGame::NPC.new(world.new_id, 'goblin', 'a pungent goblin', 'This goblin is quite rank.', true)
   player.add_item(goblin)
 
-  puts 'DEBUG init_world 99'
+  Rails.logger.info 'init_world returning'
 end
 
 init_world
